bb06e82b3efb0bb5b2e5ea6e8f92d06349fec8e2
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-09-18 13:12:33 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>BEM-VINDO A HOME</h1>
+        </div>
+    )
+}
+
+export default Home;
+
+// O nome do componente sempre é maiúscula e também o seu nome nas pastas
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+// importar a biblioteca que nos possibilita a navegação por páginas
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Filme from './pages/Filme';
+
+function RoutesApp() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={ <Home/>}/>
+            <Route path='/filme/:id' element={ <Filme/> }/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
